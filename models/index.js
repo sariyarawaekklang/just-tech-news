a08e95c39b9a5d277cbc49c617148232a8eb0091
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-26 23:15:47 -0600

    lesson 4.3

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // imports the User and Post model
-const User = require("./User");
-const Post = require("./Post");
+const User = require('./User');
+const Post = require('./Post');
+const Vote = require('./Vote')
 
 // create associations
 User.hasMany(Post, {
@@ -12,5 +13,33 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
 // exports any object with User as a property
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
