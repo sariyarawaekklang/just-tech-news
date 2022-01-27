7586d2e367545355bbfc76274b26b55e3885bbe1
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-26 22:34:40 -0600

    finish lesson 3

--- models/index.js
+++ models/index.js
@@ -2,5 +2,15 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// defines the relationship of the Post model to the User; this is so that a post can belong to one user, not many users
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 // exports any object with User as a property
 module.exports = { User, Post };
